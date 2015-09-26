7436ae621c77b27aab32592666779b7d383732f7
Jan T. Sott <jan@idleberg.com>
2015-09-26 11:44:25 +0200

    update script from Gist

--- chmod.py
+++ chmod.py
@@ -1,13 +1,25 @@
+# https://gist.github.com/idleberg/03bc3766c760bb4b81e3
+
 import os, stat, sublime, sublime_plugin
 
-# Configuration
-pkg = 'Inno Setup'
-script = sublime.packages_path() + '/' + pkg + '/inno-build.sh'
+# Package name, must match directory name
+p = 'Inno Setup'
+
+# Array of files, relative to package directory
+files = [
+    'build.sh'
+]
 
 def plugin_loaded():
     from package_control import events
 
-    # chmod +x <script>
-    if (events.install(pkg) or events.post_upgrade(pkg)) and os.name is 'posix' or 'mac':
-        st = os.stat(script)
-        os.chmod(script, st.st_mode | stat.S_IEXEC)
+    if (events.install(p) or events.post_upgrade(p)) and os.name is 'posix' or 'mac':
+        for file in files:
+
+            # Concat full path
+            f = sublime.packages_path() + '/' + p + '/' + file
+
+            # Change permissions, if file exists
+            if os.path.isfile(f):
+                st = os.stat(f)
+                os.chmod(f, st.st_mode | stat.S_IEXEC)
