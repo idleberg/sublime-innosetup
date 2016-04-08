c6776e98ea87e03f5a1f8d9094d289b1fc18a789
Csonkás Kristóf <kristof.csonkas.ext@unify.com>
2016-04-08 13:38:33 +0200

    Made the popup visibility and it's style configureable.

--- sublime-innosetup.py
+++ sublime-innosetup.py
@@ -5,24 +5,37 @@ import re
 
 iscc = ''
 exists = False
+popup_enabled = True
+popup_style = ''
+
+
+def refresh_popup_style(bg_color, text_color):
+  global popup_style
+  popup_style = (
+        '<style>'
+        'html {'
+        'background-color: ' + bg_color + '; padding: 1px '
+        '}'
+        'body { margin: 1px; color: ' + text_color + ' } '
+        '</style>'
+        )
 
 
 def plugin_loaded():
-  global iscc, exists
+  global iscc, exists, popup_enabled, popup_style
   try:
     subprocess.call(['iscc'], shell=True)
     iscc = 'iscc'
     exists = True
+    settings = sublime.load_settings('Preferences.sublime-settings')
+    popup_enabled = settings.get('inno_popup_enabled', True)
+    popup_bg = settings.get('inno_popup_bg', '#AAAAAA')
+    popup_text = settings.get('inno_popup_text', '#AA4400')
+    refresh_popup_style(popup_bg, popup_text)
   except OSError as e:
     print(e)
 
 err_lines = {}
-style = ('<style>'
-         'html {'
-         'background-color: #AAAAAA; padding: 1px '
-         '}'
-         'body { margin: 1px; color: #AA4400 } '
-         '</style>')
 
 
 class LintAction(sublime_plugin.EventListener):
@@ -32,10 +45,12 @@ class LintAction(sublime_plugin.EventListener):
   def on_selection_modified_async(self, view):
     if 'Inno' not in view.settings().get('syntax'):
       return
+    if not popup_enabled:
+      return
     sel_line = view.rowcol(view.sel()[0].a)[0]
     if sel_line in list(err_lines.keys()):
       html = '<html>' + err_lines[sel_line] + '</html>'
-      view.show_popup(style + html, max_width=500)
+      view.show_popup(popup_style + html, max_width=500)
 
 
 def lint(view):
@@ -62,3 +77,12 @@ def highlight(view, err_lines):
     line_region = view.line(view.text_point(item, 0))
     error_regions.append(line_region)
   view.add_regions('inno_error', error_regions, 'entity.name.type.class.error.inno', 'dot', sublime.DRAW_NO_FILL)
+
+
+class TogglePopup(sublime_plugin.ApplicationCommand):
+  def run(self):
+    global popup_enabled
+    settings = sublime.load_settings('Preferences.sublime-settings')
+    curr = settings.get('inno_popup_enabled', True)
+    popup_enabled = not curr
+    settings.set('inno_popup_enabled', popup_enabled)
